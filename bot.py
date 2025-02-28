a389da7ac6a9895a33ab211e6046dc777191b19e
Jesse Spielman <jesse.spielman@gmail.com>
2025-02-28 02:38:17 +0000

    Initial commit of bot based on...that guy's stuff.  Fill in later.

---
+++ bot.py
@@ -0,0 +1,11 @@
+import tracery
+import json
+from tracery.modifiers import base_english
+
+with open("rules.json") as f:
+    rules = json.loads(f.read())
+
+grammar = tracery.Grammar(rules)
+grammar.add_modifiers(base_english)
+for i in range(100):
+    print(grammar.flatten("#origin#"))
