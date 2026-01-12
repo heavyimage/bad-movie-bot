960aa6612777ddb3d1a4df07fa632759953efd36
Jesse Spielman <jesse.spielman@gmail.com>
2026-01-12 12:35:30 +0000

    Waypoint on places

--- places.py
+++ places.py
@@ -1,57 +1,20 @@
 # TODO: needs love!
 base_places_rt = {
     "trapped_place": ["slaughterhouse", "cages", "prison", "trap", "snare", "jail", "#threatening_substance#"],
-
-    "moment_in_time": ["#day_of_the_week#", "age", "era", "aeon", "eon", "day",
-                       "night", "year", "solstice", "time", "dawn", "sunrise",
-                       "dusk", "twilight", "future"],
-
-    "spooky_locale": ["mesa", "swamp", "island", "land", "park", "beach", "lake", "tower", "mountain", "hill", "lighthouse", "pyramid", "void", "barrens", "barrier", "caldera", "castle", "cave", "continent", "crater", "crypt", "dungeon", "field", "forest", "graveyard", "cemetery", "grotto", "hill", "hive", "island", "jungle", "keep", "kingdom", "lagoon", "lagoon", "lake", "mausoleum", "mine", "mountain", "rim", "ruin", "sands", "sepulcher", "shipwreck", "shores", "swamp", "throne", "tomb", "torture chamber", "tower", "valley", "vault", "volcano", "wilderness", "fortress", "labrynth", "pit", "earth", "year #funny_year#", "center of the earth", "below", "beyond", "above", "underground", "20,000 fathoms"],
-
-    "building": ["cabin", "village", "hotel", "house", "mansion", "fort",
-                 "stalag", "base", "barracks", "town", "colony", "cabin",
-                 "city", "realm", "zone", "church", "abbey", "cathedral",
-                 "pagoda", "chapel", "reliquary", "synagogue", "mosque",
-                 "temple", "hostel", "bodega", "adult cinema", "suburbs",
-                 "fair", "barn", "mall", "science fair", "DMV", "library", "harem"],
-
-    "water_and_oceans": [ "sea", "deep", "depths", "reef"],
-
+    "moment_in_time": ["#day_of_the_week#", "age", "era", "aeon", "eon", "day", "night", "year", "solstice", "time", "dawn", "sunrise", "dusk", "twilight", "future"],
+    "spooky_natural_locale": ["mesa", "swamp", "island", "land", "park", "beach", "lake", "tower", "mountain", "hill", "lighthouse", "pyramid", "void", "barrens", "barrier", "caldera", "castle", "cave", "continent", "crater", "crypt", "dungeon", "field", "forest", "graveyard", "cemetery", "grotto", "hill", "hive", "island", "jungle", "keep", "kingdom", "lagoon", "lagoon", "lake", "mausoleum", "mine", "mountain", "rim", "ruin", "sands", "sepulcher", "shipwreck", "shores", "swamp", "throne", "tomb", "torture chamber", "tower", "valley", "vault", "volcano", "wilderness", "fortress", "labrynth", "pit", "earth", "year #funny_year#", "center of the earth", "below", "beyond", "above", "underground", "20,000 fathoms", "sea", "deep", "depths", "reef"],
+    "human_structure": ["cabin", "village", "hotel", "house", "mansion", "fort", "stalag", "base", "barracks", "town", "colony", "cabin", "city", "realm", "zone", "church", "abbey", "cathedral", "pagoda", "chapel", "reliquary", "synagogue", "mosque", "temple", "hostel", "bodega", "adult cinema", "suburbs", "fair", "barn", "mall", "science fair", "DMV", "library", "harem"],
+    "scary_parts_of_a_house": [ "attic", "basement", "bed", "cellar", "closet", "fireplace", "furnace", "kitchen", "sewer", "garden", "shed", "walls", "well", "crawlspace", "panic room" ],
     "cosmic_place": ["spaceship", "space station", "astral plane", "asteroid belt", "moon", "black hole", "eclipse", "continuum", "#star# constellation", "planet", "galaxy", "nebula", "dimension", "world", "star", "solar system"],
-
-    "scary_parts_of_a_house": [
-        "attic", "basement", "bed", "cellar", "closet", "fireplace",
-        #"floorboards",
-        #"painting",
-        "furnace", 
-        "kitchen", 
-        #"room",
-        "sewer", 
-        "garden", 
-        #"backyard",
-        "shed", 
-        #"stairs", 
-        "walls", 
-        "well", 
-        "crawlspace", 
-        "panic room"
-    ],
-
-    "places_on_earth_that_use_the": [
-        "Orient", "atlantic", "far #cardinal_direction#", "nile", "north pole", "outback",
-        "Himalayas", "pacific", "antarctic", "Zambezi", "amazon"],
+    "places_on_earth_with_the": [ "Orient", "atlantic", "far #cardinal_direction#", "nile", "north pole", "outback", "Himalayas", "pacific", "antarctic", "Zambezi", "amazon"],
 
 }
 
 base_places_nrt = {
-
-    # NOTE: proper names
-	"star": [ "altair", "andromeda", "cygnus", "orion", "corona borealis", "cetus", "sol", "centauri"],
+    "star": [ "altair", "andromeda", "cygnus", "orion", "corona borealis", "cetus", "sol", "centauri"],
     "fancy_star": ["#star# #maybe_fun_numeral#", "#greek_letter# #star#"],
     "named_planetary_body": ["phobos", "deimos", "europa", "ganymede", "callisto", "titan", "hyperion", "iapetus", "io", "mars", "mercury", "neptune", "jupiter", "pluto", "saturn", "titan", "uranus", "venus", "planet x",],
     "mythological_places": ["styx", "kingdom come", "hell", "dune", "elm street", "greenwich village", "eden", "atlantis", "troy", "Olympus", "Armageddon", "valhalla", "underworld", "miskatonic university", "Innsmouth", "avalon", "Ga'Hoole", "Shangra-La", "El Dorado", "Hyboria", "carcosa", "R'lyeh", "#greek_letter# Ceti #number#", "#greek_letter# Persei #number#", "vulcan", "lemuria", "leng"],
-    "places_on_earth": ["hanging rock", "Manhattan", "Antarctica", "Babylonia", "Basque Country", "Africa", "Baghdad", "Persia", "Harlem", "Berlin", "Bavaria", "Egypt", "L.A.", "New Jersey", "Tibet", "Area 51", "Groom Lake", "Main Street USA", "Mesoptamia", "Mongolia", "Siberia", "Soviet Russia", "Tampa", "Yucca Flats", "#cardinal_direction#", "Rutgers"],
+    "places_on_earth_without_the": ["hanging rock", "Manhattan", "Antarctica", "Babylonia", "Basque Country", "Africa", "Baghdad", "Persia", "Harlem", "Berlin", "Bavaria", "Egypt", "L.A.", "New Jersey", "Tibet", "Area 51", "Groom Lake", "Main Street USA", "Mesoptamia", "Mongolia", "Siberia", "Soviet Russia", "Tampa", "Yucca Flats", "#cardinal_direction#", "Rutgers"],
     "other": ["time", "space", "forever", "eternity", "tomorrow"],
-
-
 }
